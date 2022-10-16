f54a644c7c2e991a999863e4e151d7fa261ea114
wendymoira12 <anne.diwata@gmail.com>
2022-10-17 00:14:29 +0800

    remove lang

--- warp/animal-shelter-page.php
+++ warp/animal-shelter-page.php
@@ -40,18 +40,10 @@ $row = mysqli_fetch_assoc($result);
 </head>
 
 <body>
-    <!--[if lte IE 9]>
-            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
-        <![endif]-->
 
-    <!-- header_start  -->
     </head>
 
     <body>
-        <!--[if lte IE 9]>
-            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
-        <![endif]-->
-
         <header>
             <div class="header-area ">
 
@@ -69,7 +61,7 @@ $row = mysqli_fetch_assoc($result);
                                 <div class="main-menu  d-none d-lg-block">
                                     <nav>
                                         <ul id="navigation">
-                                        <li><a href="home.php">Home</a></li>
+                                            <li><a href="home.php">Home</a></li>
                                             <li> <a href="about.html">About Us </i></a>
                                             <li> <a href="pets-for-adoption.php">Pets for Adoption </i></a>
                                             <li><a href="contact.html">Contact</a></li>
@@ -88,8 +80,7 @@ $row = mysqli_fetch_assoc($result);
                 </div>
             </div>
         </header>
-        <!-- header_start  -->
-        <!-- bradcam_area_start -->
+
         <div class="bradcam_area breadcam_bg">
             <div class="container">
                 <div class="row">
@@ -101,7 +92,7 @@ $row = mysqli_fetch_assoc($result);
                 </div>
             </div>
         </div>
-        <!-- bradcam_area_end -->
+
         <!-- service_area_start  -->
         <div class="service_area">
             <div class="container">
@@ -143,22 +134,22 @@ $row = mysqli_fetch_assoc($result);
                                             foreach ($result as $data) {
                                 ?>
 
-<div class="item" data-id="<?php echo $data['pet_specie']; ?>">
-                                    <div class="inner">
-                                        <a href="AdopteePage.php?id=<?php echo $data['pet_id']; ?>">
-                                            <img src="shelter/production/images/<?= $data['pet_img']; ?>"> </a>
-                                        <div class="service_content text-center">
-                                            <a href="AdopteePage.php?id=<?php echo $data['pet_id']; ?>">
-                                                <h3><?= $data['pet_name']; ?></h3>
-                                            </a>
-                                            <h5> <b> Gender:</b> <?= $data['pet_gender']; ?> <br>
-                                                <b> Age:</b> <?= $data['pet_age']; ?> <br>
-                                                <b> Size:</b> <?= $data['pet_size']; ?> <br>
-                                                <b> Neutered:</b> <?= $data['pet_neuter']; ?>
-                                            </h5>
-                                        </div>
-                                    </div>
-                                </div>
+                                                <div class="item" data-id="<?php echo $data['pet_specie']; ?>">
+                                                    <div class="inner">
+                                                        <a href="AdopteePage.php?id=<?php echo $data['pet_id']; ?>">
+                                                            <img src="shelter/production/images/<?= $data['pet_img']; ?>"> </a>
+                                                        <div class="service_content text-center">
+                                                            <a href="AdopteePage.php?id=<?php echo $data['pet_id']; ?>">
+                                                                <h3><?= $data['pet_name']; ?></h3>
+                                                            </a>
+                                                            <h5> <b> Gender:</b> <?= $data['pet_gender']; ?> <br>
+                                                                <b> Age:</b> <?= $data['pet_age']; ?> <br>
+                                                                <b> Size:</b> <?= $data['pet_size']; ?> <br>
+                                                                <b> Neutered:</b> <?= $data['pet_neuter']; ?>
+                                                            </h5>
+                                                        </div>
+                                                    </div>
+                                                </div>
 
                             </div>
                         </div>
@@ -179,74 +170,7 @@ $row = mysqli_fetch_assoc($result);
 
 
 
-<!-- ================ contact section start ================= -->
-<!--
-<section class="contact-section">
-        <div class="container">
-            
-            <div class="row">
-                <div class="col-12">
-                    <h2 class="contact-title">Send them a Message</h2>
-                </div>
-                <div class="col-lg-8">
-                    <form class="form-contact contact_form" action="contact_process.php" method="post" id="contactForm" novalidate="novalidate">
-                        <div class="row">
-                            <div class="col-12">
-                                <div class="form-group">
-                                    <textarea class="form-control w-100" name="message" id="message" cols="30" rows="9" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter Message'" placeholder=" Name"></textarea>
-                                </div>
-                            </div>
-                            <div class="col-sm-6">
-                                <div class="form-group">
-                                    <input class="form-control valid" name="name" id="name" type="text" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter your name'" placeholder="Enter your name">
-                                </div>
-                            </div>
-                            <div class="col-sm-6">
-                                <div class="form-group">
-                                    <input class="form-control valid" name="email" id="email" type="email" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter email address'" placeholder="Email">
-                                </div>
-                            </div>
-                            <div class="col-12">
-                                <div class="form-group">
-                                    <input class="form-control" name="subject" id="subject" type="text" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter Subject'" placeholder="Enter Subject">
-                                </div>
-                            </div>
-                        </div>
-                        <div class="form-group mt-3">
-                            <button type="submit" class="button button-contactForm boxed-btn">Send</button>
-                        </div>
-                    </form>
-                </div>
-                <div class="col-lg-3 offset-lg-1">
-                    <div class="media contact-info">
-                        <span class="contact-info__icon"><i class="ti-home"></i></span>
-                        <div class="media-body">
-                            <h3>Las Piñas, Philippines</h3>
-                          
-                        </div>
-                    </div>
-                    <div class="media contact-info">
-                        <span class="contact-info__icon"><i class="ti-tablet"></i></span>
-                        <div class="media-body">
-                            <h3>8225-2911 <br> 0965-653-6449 </h3>
-                            
-                            <p>Mon to Fri 8am to 5pm</p>
-                        </div>
-                    </div>
-                    <div class="media contact-info">
-                        <span class="contact-info__icon"><i class="ti-email"></i></span>
-                        <div class="media-body">
-                            <h3>laspinasanimalshelter@gmail.com</h3>
-                            <p>Send us your query anytime!</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
--->
-<!-- ================ contact section end ================= -->
-
+<!-- ================ footer ================= -->
 
 
 <footer class="footer">
