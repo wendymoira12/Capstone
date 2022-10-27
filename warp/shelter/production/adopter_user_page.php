a35267b2364eb339d23b79d308b9d9e243d8f710
Obiasfate <79243642+Obiasfate@users.noreply.github.com>
2022-10-28 02:06:30 +0800

    Update adopter_user_page.php
    Removed unnecessary queries, fixed table and queries

--- warp/shelter/production/adopter_user_page.php
+++ warp/shelter/production/adopter_user_page.php
@@ -4,7 +4,7 @@ session_start();
 
 //if hindi nakaset si user-email and user-role-id babalik sya sa login.php
 
-if (!isset($_SESSION['user-email'], $_SESSION['user-role-id'],$_SESSION['user_id'])) {
+if (!isset($_SESSION['user-email'], $_SESSION['user-role-id'], $_SESSION['user_id'])) {
   header('Location:/Capstone/warp/login.php');
 } else {
   $role_id = $_SESSION['user-role-id'];
@@ -15,10 +15,6 @@ if (!isset($_SESSION['user-email'], $_SESSION['user-role-id'],$_SESSION['user_id
   }
 }
 
-$user_id = $_SESSION['user_id'];
-$sql = "SELECT * FROM user_tbl WHERE user_id ='$user_id'";
-$result = mysqli_query($conn, $sql);
-$row1 = mysqli_fetch_assoc($result);
 
 $user_id = $_SESSION['user_id'];
 $sql = "SELECT * FROM adopter_tbl WHERE user_id ='$user_id'";
@@ -27,45 +23,17 @@ $result = mysqli_query($conn, $sql);
 if ($result->num_rows > 0) {
   $row = mysqli_fetch_assoc($result);
   $adopter_id = $row['adopter_id'];
-  $sql = "SELECT * FROM user_tbl INNER JOIN adopter_tbl ON user_tbl.user_id = adopter_tbl.user_id WHERE user_tbl.user_id AND adopter_tbl.user_id ='$adopter_id'";
-  $result = mysqli_query($conn, $sql);
-  if ($result == TRUE) {
-    $adopterresult = mysqli_fetch_assoc($result);
-  }
 }
 ?>
 
 <?php
-$sqlapp= "SELECT * FROM applicationform1, applicationresult_tbl WHERE adopter_id ='$adopter_id';";
+$i = 1;
+//Columns needed to query = No., Date Submitted, Adopter Name, Adoptee name, Application Status
+$sqlapp = "SELECT applicationform1.date_submitted, applicationform1.adopter_id, applicationform1.pet_id, applicationresult_tbl.application_status, applicationform1.application_id, adoptee_tbl.pet_name FROM applicationform1 INNER JOIN applicationresult_tbl ON applicationform1.application_id = applicationresult_tbl.application_id INNER JOIN adoptee_tbl ON applicationform1.pet_id = adoptee_tbl.pet_id INNER JOIN adopter_tbl ON applicationform1.adopter_id = adopter_tbl.adopter_id WHERE adopter_tbl.adopter_id = '$adopter_id'";
+
+//$sqlapp= "SELECT * FROM applicationform1, applicationresult_tbl WHERE adopter_id ='$adopter_id';";
 
 $result = mysqli_query($conn, $sqlapp);
-
-if ($result) {
-    if (mysqli_num_rows($result) > 0) {
-      $row1 = mysqli_fetch_assoc($result);
-    } else {
-        echo 'not found';
-    }
-} else {
-    echo 'Error: ' . mysqli_error();
-}
-
-//mysqli_close($mysqli);
-
-?>
-<?php
-
-$i = 1;
-$sql = "SELECT * FROM adoptee_tbl, applicationform1 WHERE adoptee_tbl.pet_id = applicationform1.pet_id;";
-$result = $conn->query($sql);
-$data = mysqli_fetch_assoc($result);
-
-?>
-<?php
-
-
-//mysqli_close($mysqli);
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -142,93 +110,93 @@ $data = mysqli_fetch_assoc($result);
       </div>
 
       <!-- top navigation -->
-        <div class="top_nav">
-            <div class="nav_menu">
-              <nav>
-                <div class="nav toggle">
-                  <a id="menu_toggle"><i class="fa fa-bars"></i></a>
-                </div>
-
-                <ul class="nav navbar-nav navbar-right">
-                  <li class="">
-                    
-                    <ul class="dropdown-menu dropdown-usermenu pull-right">
-                      <li><a href="logout.php?logout"><i class="fa fa-sign-out pull-right"></i> Log Out</a></li>
-                    </ul>
-                  </li>
-                  <li><a href="logout.php?logout">Logout </i></a>
-                  <li> <a href="/Capstone/warp/home.php">Go to Homepage </i></a>
-              <!-- Notification bell -->
-              
-              <li role="presentation" class="dropdown">
-                  <a href="javascript:;" class="dropdown-toggle info-number" data-toggle="dropdown" aria-expanded="false">
-                    <i class="fa fa-bell-o"></i>
-                    <span class="badge bg-green">6</span>
-                  </a>
-                  <ul id="menu1" class="dropdown-menu list-unstyled msg_list" role="menu">
-                    <li>
-                      <a>
-                        <span class="image"><img src="images/img.jpg" alt="Profile Image" /></span>
-                        <span>
-                          <span>John Smith</span>
-                          <span class="time">3 mins ago</span>
-                        </span>
-                        <span class="message">
-                          Film festivals used to be do-or-die moments for movie makers. They were where...
-                        </span>
-                      </a>
-                    </li>
-                    <li>
-                      <a>
-                        <span class="image"><img src="images/img.jpg" alt="Profile Image" /></span>
-                        <span>
-                          <span>John Smith</span>
-                          <span class="time">3 mins ago</span>
-                        </span>
-                        <span class="message">
-                          Film festivals used to be do-or-die moments for movie makers. They were where...
-                        </span>
-                      </a>
-                    </li>
-                    <li>
-                      <a>
-                        <span class="image"><img src="images/img.jpg" alt="Profile Image" /></span>
-                        <span>
-                          <span>John Smith</span>
-                          <span class="time">3 mins ago</span>
-                        </span>
-                        <span class="message">
-                          Film festivals used to be do-or-die moments for movie makers. They were where...
-                        </span>
-                      </a>
-                    </li>
-                    <li>
-                      <a>
-                        <span class="image"><img src="images/img.jpg" alt="Profile Image" /></span>
-                        <span>
-                          <span>John Smith</span>
-                          <span class="time">3 mins ago</span>
-                        </span>
-                        <span class="message">
-                          Film festivals used to be do-or-die moments for movie makers. They were where...
-                        </span>
-                      </a>
-                    </li>
-                    <li>
-                      <div class="text-center">
-                        <a>
-                          <strong>See All Alerts</strong>
-                          <i class="fa fa-angle-right"></i>
-                        </a>
-                      </div>
-                    </li>
-                  </ul>
-                </li>
-
-                  
-              </nav>
+      <div class="top_nav">
+        <div class="nav_menu">
+          <nav>
+            <div class="nav toggle">
+              <a id="menu_toggle"><i class="fa fa-bars"></i></a>
             </div>
-          </div>
+
+            <ul class="nav navbar-nav navbar-right">
+              <li class="">
+
+                <ul class="dropdown-menu dropdown-usermenu pull-right">
+                  <li><a href="logout.php?logout"><i class="fa fa-sign-out pull-right"></i> Log Out</a></li>
+                </ul>
+              </li>
+              <li><a href="logout.php?logout">Logout </i></a>
+              <li> <a href="/Capstone/warp/home.php">Go to Homepage </i></a>
+                <!-- Notification bell -->
+
+              <li role="presentation" class="dropdown">
+                <a href="javascript:;" class="dropdown-toggle info-number" data-toggle="dropdown" aria-expanded="false">
+                  <i class="fa fa-bell-o"></i>
+                  <span class="badge bg-green">6</span>
+                </a>
+                <ul id="menu1" class="dropdown-menu list-unstyled msg_list" role="menu">
+                  <li>
+                    <a>
+                      <span class="image"><img src="images/img.jpg" alt="Profile Image" /></span>
+                      <span>
+                        <span>John Smith</span>
+                        <span class="time">3 mins ago</span>
+                      </span>
+                      <span class="message">
+                        Film festivals used to be do-or-die moments for movie makers. They were where...
+                      </span>
+                    </a>
+                  </li>
+                  <li>
+                    <a>
+                      <span class="image"><img src="images/img.jpg" alt="Profile Image" /></span>
+                      <span>
+                        <span>John Smith</span>
+                        <span class="time">3 mins ago</span>
+                      </span>
+                      <span class="message">
+                        Film festivals used to be do-or-die moments for movie makers. They were where...
+                      </span>
+                    </a>
+                  </li>
+                  <li>
+                    <a>
+                      <span class="image"><img src="images/img.jpg" alt="Profile Image" /></span>
+                      <span>
+                        <span>John Smith</span>
+                        <span class="time">3 mins ago</span>
+                      </span>
+                      <span class="message">
+                        Film festivals used to be do-or-die moments for movie makers. They were where...
+                      </span>
+                    </a>
+                  </li>
+                  <li>
+                    <a>
+                      <span class="image"><img src="images/img.jpg" alt="Profile Image" /></span>
+                      <span>
+                        <span>John Smith</span>
+                        <span class="time">3 mins ago</span>
+                      </span>
+                      <span class="message">
+                        Film festivals used to be do-or-die moments for movie makers. They were where...
+                      </span>
+                    </a>
+                  </li>
+                  <li>
+                    <div class="text-center">
+                      <a>
+                        <strong>See All Alerts</strong>
+                        <i class="fa fa-angle-right"></i>
+                      </a>
+                    </div>
+                  </li>
+                </ul>
+              </li>
+
+
+          </nav>
+        </div>
+      </div>
       <!-- page content -->
       <div class="right_col" role="main">
         <div class="">
@@ -242,23 +210,22 @@ $data = mysqli_fetch_assoc($result);
             <div class="clearfix"></div>
 
             <div class="row">
-              <div class="col-md-6 col-sm-6 col-xs-12">
+            <div class="col-md-12 col-sm-12 col-xs-12">
                 <div class="x_panel">
                   <div class="x_title">
-                    <!-- <h2>Basic Tables <small>basic table subtitle</small></h2> -->
+                    <h2>Applications</h2>
 
                     <div class="clearfix"></div>
                   </div>
                   <div class="x_content">
-
-                    <table class="table">
+                    <table id="datatable" class="table table-striped table-bordered">
                       <thead>
                         <tr>
                           <th>No.</th>
                           <th>Application ID</th>
                           <th>Pet Name</th>
-                          <th>Date</th>
-                          <th>Status</th>
+                          <th>Date Submitted</th>
+                          <th>Application Status</th>
                           <th>Action</th>
                         </tr>
                       </thead>
@@ -267,60 +234,77 @@ $data = mysqli_fetch_assoc($result);
                         foreach ($result as $data) {
 
                       ?>
-                      <tbody>
-                        <tr>
-                          <th><?= $i++ ; ?></th>
-                          <th><?php echo $row1['application_id']; ?></th>
-                          <th><?php echo $data['pet_name']; ?></th>
-                          <td><?php echo $data['date_submitted']; ?></td>
-                          <td><?php echo $row1['application_status']; ?></td>
-                          <td>
-                            <a href="adopter_application_view.php?id=<?= $row1['application_id']; ?>">
-                              <button type="button" class="btn btn-round btn-success">View</button>
-                            </a>
-                          </td>
-                        </tr>
-                      </tbody>
-                        <?php
+                          <tbody>
+                            <tr>
+                              <th><?= $i++; ?></th>
+                              <th><?php echo $data['application_id']; ?></th>
+                              <th><?php echo $data['pet_name']; ?></th>
+                              <td><?php echo $data['date_submitted']; ?></td>
+                              <td><?php echo $data['application_status']; ?></td>
+                              <td>
+                                <a href="adopter_application_view.php?id=<?= $row1['application_id']; ?>">
+                                  <button type="button" class="btn btn-round btn-success">View</button>
+                                </a>
+                              </td>
+                            </tr>
+                          </tbody>
+                      <?php
+                        }
                       }
-                    } 
-                     
-                    ?>
+
+                      ?>
                     </table>
 
                   </div>
                 </div>
               </div>
-
             </div>
           </div>
         </div>
-        <!-- /page content -->
-
-        <!-- footer content -->
-        <footer>
-          <div class="pull-right">
-            <!-- Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a> -->
-          </div>
-          <div class="clearfix"></div>
-        </footer>
-        <!-- /footer content -->
       </div>
+      <!-- /page content -->
+
+      <!-- footer content -->
+      <footer>
+        <div class="pull-right">
+          <!-- Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a> -->
+        </div>
+        <div class="clearfix"></div>
+      </footer>
+      <!-- /footer content -->
     </div>
+  </div>
 
-    <!-- jQuery -->
-    <script src="../vendors/jquery/dist/jquery.min.js"></script>
-    <!-- Bootstrap -->
-    <script src="../vendors/bootstrap/dist/js/bootstrap.min.js"></script>
-    <!-- FastClick -->
-    <script src="../vendors/fastclick/lib/fastclick.js"></script>
-    <!-- NProgress -->
-    <script src="../vendors/nprogress/nprogress.js"></script>
-    <!-- iCheck -->
-    <script src="../vendors/iCheck/icheck.min.js"></script>
+  <!-- jQuery -->
+  <script src="../vendors/jquery/dist/jquery.min.js"></script>
+  <!-- Bootstrap -->
+  <script src="../vendors/bootstrap/dist/js/bootstrap.min.js"></script>
+  <!-- FastClick -->
+  <script src="../vendors/fastclick/lib/fastclick.js"></script>
+  <!-- NProgress -->
+  <script src="../vendors/nprogress/nprogress.js"></script>
+  <!-- iCheck -->
+  <script src="../vendors/iCheck/icheck.min.js"></script>
 
-    <!-- Custom Theme Scripts -->
-    <script src="../build/js/custom.min.js"></script>
+  <!-- Datatables -->
+  <script src="../vendors/datatables.net/js/jquery.dataTables.min.js"></script>
+  <script src="../vendors/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
+  <script src="../vendors/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
+  <script src="../vendors/datatables.net-buttons-bs/js/buttons.bootstrap.min.js"></script>
+  <script src="../vendors/datatables.net-buttons/js/buttons.flash.min.js"></script>
+  <script src="../vendors/datatables.net-buttons/js/buttons.html5.min.js"></script>
+  <script src="../vendors/datatables.net-buttons/js/buttons.print.min.js"></script>
+  <script src="../vendors/datatables.net-fixedheader/js/dataTables.fixedHeader.min.js"></script>
+  <script src="../vendors/datatables.net-keytable/js/dataTables.keyTable.min.js"></script>
+  <script src="../vendors/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
+  <script src="../vendors/datatables.net-responsive-bs/js/responsive.bootstrap.js"></script>
+  <script src="../vendors/datatables.net-scroller/js/dataTables.scroller.min.js"></script>
+  <script src="../vendors/jszip/dist/jszip.min.js"></script>
+  <script src="../vendors/pdfmake/build/pdfmake.min.js"></script>
+  <script src="../vendors/pdfmake/build/vfs_fonts.js"></script>
+
+  <!-- Custom Theme Scripts -->
+  <script src="../build/js/custom.min.js"></script>
 </body>
 
 </html>
