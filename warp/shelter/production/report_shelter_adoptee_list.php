6a996a83e87ea3badddcbbcb713c991e35326d46
wendymoira12 <anne.diwata@gmail.com>
2022-12-13 16:20:09 +0800

    fix title

--- warp/shelter/production/report_shelter_adoptee_list.php
+++ warp/shelter/production/report_shelter_adoptee_list.php
@@ -44,7 +44,7 @@ if ($result->num_rows > 0) {
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <title>Animal Shelter | Adoptee Pet List
+  <title> <?php echo $row['city_name']; ?> | Adoptee Pet List
     <?php echo $city_id ?>
   </title>
   <link rel="shortcut icon" type="image/x-icon" href="/warp/img/WARP_LOGO copy.png">
