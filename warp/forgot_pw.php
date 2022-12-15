e571813bd5d6d4224aa40b510a381eee412c94d4
Obiasfate <79243642+Obiasfate@users.noreply.github.com>
2022-12-15 11:40:27 +0800

    Update forgot_pw.php

--- warp/forgot_pw.php
+++ warp/forgot_pw.php
@@ -13,64 +13,62 @@ if (isset($_POST["forgot-pw"])) {
 
   // If emailErr is empty, execute query
   if (empty($emailErr)) {
-    $sql = "SELECT * FROM user_tbl WHERE user_email = ? AND deleted_at IS NULL ";
+    $sql = "SELECT user_email FROM user_tbl WHERE user_email = ? AND deleted_at IS NULL ";
     $stmt = mysqli_stmt_init($conn);
     if (!mysqli_stmt_prepare($stmt, $sql)) {
       echo '<script>alert("SQL Prepared Statement Failed")</script>';
     } else {
       mysqli_stmt_bind_param($stmt, "s", $email);
-      if (mysqli_stmt_execute($stmt)) {
-        $result = mysqli_stmt_get_result($stmt);
-        $rowCount = mysqli_num_rows($result);
-        // If the query is true, email already exists
-        if ($rowCount > 1) {
+      mysqli_stmt_execute($stmt);
+      $result = mysqli_stmt_get_result($stmt);
+      // If the query is true, email already exists
+      if (!$result ->num_rows > 0) {
 ?>
-          <script>
-            alert("User with email doesn't exist!");
-          </script>
-          <?php
-        } else {
-          $otp = rand(100000, 999999);
-          $_SESSION['otp'] = $otp;
-          $_SESSION['mail'] = $email;
-          require "Mail/phpmailer/PHPMailerAutoload.php";
-          $mail = new PHPMailer;
+        <script>
+          alert("User with email doesn't exist!");
+        </script>
+        <?php
+      } else {
+        $otp = rand(100000, 999999);
+        $_SESSION['otp'] = $otp;
+        $_SESSION['mail'] = $email;
+        require "Mail/phpmailer/PHPMailerAutoload.php";
+        $mail = new PHPMailer;
 
-          $mail->isSMTP();
-          $mail->Host = 'smtp.gmail.com';
-          $mail->Port = 587;
-          $mail->SMTPAuth = true;
-          $mail->SMTPSecure = 'tls';
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->Port = 587;
+        $mail->SMTPAuth = true;
+        $mail->SMTPSecure = 'tls';
 
-          $mail->Username = 'warp.pup@gmail.com';
-          $mail->Password = 'zulriaypcszobrgp';
+        $mail->Username = 'warp.pup@gmail.com';
+        $mail->Password = 'zulriaypcszobrgp';
 
-          $mail->setFrom('warp.pup@gmail.com', 'WARP');
-          $mail->addAddress($_POST["email"]);
+        $mail->setFrom('warp.pup@gmail.com', 'WARP');
+        $mail->addAddress($_POST["email"]);
 
-          $mail->isHTML(true);
-          $mail->Subject = "WARP: Forgot Password";
-          $mail->Body = "<p>Dear user, </p> 
+        $mail->isHTML(true);
+        $mail->Subject = "WARP: Forgot Password";
+        $mail->Body = "<p>Dear user, </p> 
       It seems like you forgot your password for WARP. If this is true, enter the code below to reset your password<h3> $otp <br></h3>
                     <p> If you didn't request this, you can disregard this email or you can let us know.</p>
                     <br><br>
                     <p>Welcome to WARP!</p>
                     <b>WARP Team</b>";
 
-          if (!$mail->send()) {
-          ?>
-            <script>
-              alert("<?php echo "Email doesn't Exist " ?>");
-            </script>
-          <?php
-          } else {
-          ?>
-            <script>
-              alert("<?php echo "Verification code sent to " . $email ?>");
-              window.location.replace('security_code.php');
-            </script>
+        if (!$mail->send()) {
+        ?>
+          <script>
+            alert("<?php echo "Email doesn't Exist " ?>");
+          </script>
+        <?php
+        } else {
+        ?>
+          <script>
+            alert("<?php echo "Verification code sent to " . $email ?>");
+            window.location.replace('security_code.php');
+          </script>
 <?php
-          }
         }
       }
     }
